79f2072dbd45bbe5c1f3318d853709f166a45a8e
minhhtt2 <minhhtt2@fpt.edu.vn>
2025-09-21 21:44:19 +0700

    Chuan bi lab4 mâu sinh vien

---
+++ Lab4/src/main/java/com/mycompany/lab4/gvthem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab4;
+
+/**
+ *
+ * @author ICT
+ */
+public class gvthem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
